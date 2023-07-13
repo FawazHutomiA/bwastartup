f897fbd77adf1a93fe4fce30b9448c51c416df2a
FawazHutomiA <hutomifawaza@gmail.com>
2023-07-13 10:57:41 +0700

    add: middleware

--- handler/user.go
+++ handler/user.go
@@ -144,7 +144,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	userID := 5
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
 	err = c.SaveUploadedFile(file, path)
